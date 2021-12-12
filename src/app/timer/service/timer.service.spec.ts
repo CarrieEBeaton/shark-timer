61f7cc8be412366d8e90e37c2c42b1dfc894d7aa
Beaton <carriebeaton@rocketmortgage.com>
2021-12-11 22:29:39 -0500

    service tests

--- src/app/timer/service/timer.service.spec.ts
+++ src/app/timer/service/timer.service.spec.ts
@@ -1,10 +1,10 @@
-import { fakeAsync, flush, TestBed } from "@angular/core/testing";
-import { provideMockStore } from "@ngrx/store/testing";
-import { of } from "rxjs";
+import { TestBed } from "@angular/core/testing";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
 import { TimerService } from "./timer.service";
 
 describe('TimerService', () => {
     let service: TimerService;
+    let store: MockStore;
   
     beforeEach(() => {
       TestBed.configureTestingModule({
@@ -14,6 +14,7 @@ describe('TimerService', () => {
         ]
       });
       service = TestBed.inject(TimerService);
+      store = TestBed.inject(MockStore);
     });
   
     it('should be created', () => {
@@ -63,7 +64,7 @@ describe('TimerService', () => {
         expect(stopwatchStart).not.toHaveBeenCalled();
     });
 
-    it('should call next for timerStart when timer active and stop is called', () => {
+    it('should call next for stopwatchStart when timer not active and stop is called', () => {
         const timerStart = spyOn(service.timerStart$, 'next');
         const stopwatchStart = spyOn(service.stopwatchStart$, 'next');
         service.stop(undefined);
@@ -71,4 +72,70 @@ describe('TimerService', () => {
         expect(timerStart).not.toHaveBeenCalled();
     });
 
+    it('should call next for timerReset when timer active and stop is called', () => {
+        const timerReset = spyOn(service.timerReset$, 'next');
+        const stopwatchReset = spyOn(service.stopwatchReset$, 'next');
+        const dispatch = spyOn(store, 'dispatch');
+        service.reset(true);
+        expect(timerReset).toHaveBeenCalledWith(0);
+        expect(stopwatchReset).not.toHaveBeenCalled();
+        expect(dispatch).not.toHaveBeenCalled();
+    });
+    
+    it('should call next for stopwatchReset when timer not active and stop is called', () => {
+        const timerReset = spyOn(service.timerReset$, 'next');
+        const stopwatchReset = spyOn(service.stopwatchReset$, 'next');
+        const dispatch = spyOn(store, 'dispatch');
+        service.reset(undefined);
+        expect(timerReset).not.toHaveBeenCalled();
+        expect(stopwatchReset).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenCalled();
+    });
+
+    it('should call next for timerEnd end is called', () => {
+        const timerEnd = spyOn(service.timerEnd$, 'next');
+        const startAlarm = spyOn(service, 'startAlarm');
+        service.end(false);
+        expect(timerEnd).toHaveBeenCalled();
+        expect(startAlarm).not.toHaveBeenCalled();
+    });
+
+    it('should call next for timerEnd end is called and start alarm when timer complete', () => {
+        const timerEnd = spyOn(service.timerEnd$, 'next');
+        const startAlarm = spyOn(service, 'startAlarm');
+        service.end(true);
+        expect(timerEnd).toHaveBeenCalled();
+        expect(startAlarm).toHaveBeenCalled();
+    });
+
+    
+    it('should call next for alarmEnabled when toggleAlarm is called', () => {
+        const alarmEnabled = spyOn(service.alarmEnabled$, 'next');
+        service.toggleAlarm();
+        expect(alarmEnabled).toHaveBeenCalled();
+    });
+
+    it('should call next for alarmSounding when startAlarm is called', () => {
+        const alarmSounding = spyOn(service.alarmSounding$, 'next');
+        service.startAlarm();
+        expect(alarmSounding).toHaveBeenCalled();
+    });
+
+    it('should call next for alarmSounding when startAlarm is called', () => {
+        const alarmSounding = spyOn(service.alarmSounding$, 'next');
+        service.startAlarm();
+        expect(alarmSounding).toHaveBeenCalledWith(true);
+    });
+
+    it('should call next for alarmSounding when stopAlarm is called', () => {
+        const alarmSounding = spyOn(service.alarmSounding$, 'next');
+        service.stopAlarm();
+        expect(alarmSounding).toHaveBeenCalledWith(false);
+    });
+
+    it('should call next for fullScreen when toggleFullscreen is called', () => {
+        const fullScreen = spyOn(service.fullScreen$, 'next');
+        service.toggleFullscreen();
+        expect(fullScreen).toHaveBeenCalledWith(!service.fullScreen$.value);
+    });
 });
